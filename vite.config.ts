bdf15f97b25ca00cae07cb14ada46bfd35096f37
arafat-ahmed-dev <arafatahmedshlbd194@gmail.com>
2025-08-19 16:05:30 +0600

    Remove vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import {reactRouter} from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import {defineConfig} from "vite";
-import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-    plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), react()],
+    plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
     server: {
         watch: {
             ignored: [
@@ -16,6 +15,5 @@ export default defineConfig({
                 '**/dist/**'
             ]
         }
-    },
-    base: "/"
+    }
 });
